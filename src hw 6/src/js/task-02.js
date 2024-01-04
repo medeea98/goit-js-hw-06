41402c178dd9243f86d863ea7d96aaacaac58ce9
medeea98 <142827871+medeea98@users.noreply.github.com>
2024-01-04 16:50:25 +0200

    task 2

--- src hw 6/src/js/task-02.js
+++ src hw 6/src/js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulElement = document.querySelector('#ingredients');
+ingredients.forEach(ingredient => {
+  const liElement = document.createElement('li');
+  liElement.textContent = ingredient;
+  liElement.classList.add('item');
+  ulElement.append(liElement);
+});
