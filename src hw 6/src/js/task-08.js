f4026b232e111e5757bb2cd49b7a31a50a8237d2
medeea98 <142827871+medeea98@users.noreply.github.com>
2024-01-05 14:57:16 +0200

    task 8

--- src hw 6/src/js/task-08.js
+++ src hw 6/src/js/task-08.js
@@ -1,1 +1,17 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    const login = document.querySelector('.login-form');
+    login.addEventListener('submit', function(event) {
+      event.preventDefault();
+      const formData = {
+        email: this.elements.email.value,
+        password: this.elements.password.value
+      };
+      if (!formData.email || !formData.password) {
+        alert('Toate câmpurile trebuie completate!');
+        return;
+      }
+      console.log(formData);
+      this.reset();
+    });
+  });
+  
