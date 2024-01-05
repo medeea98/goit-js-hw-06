da7454f9cc557869461c3f8142314456f6f8d115
medeea98 <142827871+medeea98@users.noreply.github.com>
2024-01-05 15:31:11 +0200

    task 9

--- src hw 6/src/js/task-09.js
+++ src hw 6/src/js/task-09.js
@@ -1,5 +1,16 @@
+const changeColor = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+const bodyElement = document.body;
+changeColor.addEventListener('click', function() {
+  const randomColor = getRandomHexColor();
+  bodyElement.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
+
+
