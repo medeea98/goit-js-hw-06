0998d1dacf0cbe9e398795d119ab83f8c4e0c2ab
medeea98 <142827871+medeea98@users.noreply.github.com>
2024-01-07 15:31:14 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", () => {
+  const input = document.querySelector('#controls input[type="number"]').value;
+  createBoxes(input);
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  boxes.innerHTML = '';
+  const resultedBoxes = Array.from({ length: amount }).map((_, index) => {
+    const dimension = 30 + index * 10;
+    const box = document.createElement('div');
+    box.style.width = box.style.height = `${dimension}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    return box;
+  });
+
+  resultedBoxes.forEach(box => boxes.appendChild(box));
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
